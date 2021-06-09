c11697f60ccf95f3303950a814e15522ceb78f12
Nick Kamer <nick@ziaomedia.com>
2021-06-09 18:56:03 +0200

    fix: fixed a rare bug that could cause the conveyor to stop prematurely (and other stuff)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { spawn } from "child_process";
 
-export const ConveyorStop = Symbol("stop");
+// export const ConveyorStop = Symbol("stop");
 // export const ConveyorPause = Symbol('pause');
 
 // <I NextInput, N NextResult, F FinishResult>>
@@ -9,6 +9,7 @@ interface Config<I, N, F> {
 	args?: string[];
 	cwd?: string;
 	parseJson?: boolean;
+	debug?: boolean;
 	onNext(input: I): Promise<N>;
 	onFinish?(results: N[] /*, errors: any[]*/): F | Promise<F>;
 
@@ -33,8 +34,24 @@ export async function Conveyor<I = string, N = void, F = void>(config: Config<I,
 	const buffer: string[] = [];
 	const results: N[] = [];
 
+	let tempBuffer: string = "";
+
 	command.stdout.on("data", (data: Buffer) => {
-		buffer.push(data.toString());
+		const string = data.toString();
+		if (config.debug) console.log("Stdout:", string);
+
+		tempBuffer += string;
+
+		while (tempBuffer.includes("\n")) {
+			const index = tempBuffer.indexOf("\n");
+			const part = tempBuffer.slice(0, index).trim();
+			if (part) buffer.push(part);
+			tempBuffer = tempBuffer.slice(index + 1);
+		}
+	});
+
+	command.stderr.on("data", (data: Buffer) => {
+		if (config.debug) console.log("Stderr:", data.toString());
 	});
 
 	let running = false;
@@ -46,6 +63,8 @@ export async function Conveyor<I = string, N = void, F = void>(config: Config<I,
 		const nextValue = buffer.shift()!;
 
 		if (config.parseJson) {
+			// if (config.debug) console.log("Parsing", nextValue);
+			// Todo: wrap in trycatch and log the value that couldn't be parsed
 			const data = JSON.parse(nextValue) as I;
 			const result = await config.onNext(data);
 			results.push(result);
@@ -57,7 +76,7 @@ export async function Conveyor<I = string, N = void, F = void>(config: Config<I,
 		running = false;
 	};
 
-	while (command.exitCode === null || running) {
+	while (command.exitCode === null || running || buffer.length) {
 		if (buffer.length && !running) await next();
 		else await new Promise((r) => setTimeout(r, 100));
 	}
